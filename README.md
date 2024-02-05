270eeea0bd94fa7e35ee30b177de7097dcbb8cec
Luis <luis.milanese@gmail.com>
2024-02-04 23:45:57 -0300

    lesson 93

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-README.md
+# README.md
 https://rest-apis-flask.teclado.com/
-docker run -p 5000:5000 -w /app -v "$(pwd):/app" rest-apis-with-flask-and-python-in-2023
+
+## Build da imagem
+```docker build -t rest-apis-with-flask-and-python-in-2023 .```
+
+## Rodando container
+```docker run -p 5000:5000 -w /app -v "$(pwd):/app" rest-apis-with-flask-and-python-in-2023```
--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required
 
 from db import db
 from models.item import ItemModel
@@ -15,6 +16,7 @@ class Item(MethodView):
     def get(self, item_id):
         return ItemModel.query.get_or_404(item_id)
 
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -22,6 +24,7 @@ class Item(MethodView):
 
         return {"message": "Item deleted."}
 
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
@@ -40,12 +43,14 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         # return {"items": list(items.values())}
         # Não precisamos mais fazer o cast para uma list pq Schema fará
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
